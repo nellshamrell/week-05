0c26d3e75a4e718960456c95c31ea2dafdbe7233
Nell Shamrell <nshamrell@bluebox.net>
2011-11-06 20:11:24 -0800

    fixed all the broken tests but two

--- lib/quotes.rb
+++ lib/quotes.rb
@@ -16,6 +16,51 @@
 # @see http://rubydoc.info/stdlib/core/1.9.2/Module#alias_method-instance_method
 # 
 class Quotes
-  
-  
+
+  attr_reader :file
+  attr_reader :params
+  attr_reader :line
+
+  def initialize(file)
+    file.each { |k,v| @file = v}
+  end
+
+  def find(line)
+    return "Could not find a quote at this time" unless File.exist?(@file)
+    line_count = 0
+    File.open(@file) {|f| line_count = f.read.count("\n")}
+    line = Random.rand(line_count + 1) if line > line_count
+    text = File.open(@file).readlines
+    quote = text[line].chomp 
+    return quote
+  end
+
+  def all
+
+   results = []
+
+    File.open(@file) do |f|
+      f.each do |line|
+        line.chomp!
+        results << line
+      end
+    end    
+    return results
+  end
+
+  def quotes
+    results = all
+  end
+
+  def search(params ={})
+    results = []
+    return results  unless File.exist?(@file)
+    
+    quotes.map do |quote|
+       params.empty? ? quote : params.map {|key,value| quote if quote.send("#{key}?", value) }.uniq
+    end.flatten.compact
+  end
+
+  alias :[] :find
 end
+
